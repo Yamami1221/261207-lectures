04455d0d6278c87903316777d85f192330acafa9
Yamami1221 <anak2547@gmail.com>
2023-08-31 14:35:07 +0700

    install mantine form and add validation

--- lecture15-2566-starter/src/app/page.js
+++ lecture15-2566-starter/src/app/page.js
@@ -16,9 +16,39 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { runningPlans } from "../libs/runningPlans";
+import { useForm, zodResolver } from "@mantine/form";
+import { z } from "zod";
+
+const schema = z.object({
+  firstName: z.string().min(3, "Required at least 3 characters"),
+  lastName: z.string().min(3, "Required at least 3 characters"),
+  email: z.string().email(),
+  plan: z.enum([...runningPlans.map((plan) => plan.value)], {
+    errorMap: () => ({ message: "Please select plan" }),
+  }),
+  gender: z.enum(["male", "female"], {
+    errorMap: () => ({ message: "please select your gender"}),
+  }),
+  acceptTerms: z.boolean().refine((val) => val === true, {
+    message: "Please accept terms and conditions",
+  }),
+});
 
 export default function Home() {
   const [opened, { open, close }] = useDisclosure(false);
+
+  const form = useForm({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      plan: null,
+      gender: null,
+      acceptTerms: false,
+    },
+    validateInputOnChange: true,
+    validate: zodResolver(schema),
+  });
 
   return (
     <div>
@@ -26,37 +56,41 @@ export default function Home() {
         <Title italic align="center" color="pink">
           Register CMU Marathon 🥈
         </Title>
-        <Stack spacing="sm">
-          <Group grow align="start">
-            <TextInput label="First Name" />
-            <TextInput label="Last Name" />
-          </Group>
-          <TextInput label="Email" />
-          <Select
-            label="Plan"
-            data={runningPlans}
-            placeholder="Please select plan..."
-          />
-          <Radio.Group label="Gender">
-            <Radio value="male" label="Male 👨" mb="xs" />
-            <Radio value="female" label="Female 👧" />
-          </Radio.Group>
+        <form onSubmit={form.onSubmit(() => {alert('fuck you')})} >
+          <Stack spacing="sm">
+            <Group grow align="start">
+              <TextInput label="First Name" {...form.getInputProps('firstName')} />
+              <TextInput label="Last Name" {...form.getInputProps('lastName')} />
+            </Group>
+            <TextInput label="Email" {...form.getInputProps('email')} />
+            <Select
+              label="Plan"
+              data={runningPlans}
+              placeholder="Please select plan..."
+              {...form.getInputProps('plan')}
+            />
+            <Radio.Group label="Gender" {...form.getInputProps('gender')}>
+              <Radio value="male" label="Male 👨" mb="xs" />
+              <Radio value="female" label="Female 👧" />
+            </Radio.Group>
 
-          <Checkbox
-            label={
-              <Text>
-                I accept{" "}
-                <Anchor onClick={open} href="#">
-                  terms and conditions
-                </Anchor>
-              </Text>
-            }
-          />
-          <Button>Register</Button>
-        </Stack>
+            <Checkbox
+              {...form.getInputProps('acceptTerms')}
+              label={
+                <Text>
+                  I accept{" "}
+                  <Anchor onClick={open} href="#">
+                    terms and conditions
+                  </Anchor>
+                </Text>
+              }
+            />
+            <Button type="submit">Register</Button>
+          </Stack>
+        </form>
       </Container>
 
       <TermsAndCondsModal opened={opened} close={close} />
-    </div>
+    </div >
   );
 }
