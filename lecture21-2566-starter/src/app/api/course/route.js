bc241836d0f687b40efef7cb7f233aab89b16676
Yamami1221 <anak2547@gmail.com>
2023-09-25 15:07:12 +0700

    add lecture21-2566-starter

---
+++ lecture21-2566-starter/src/app/api/course/route.js
@@ -0,0 +1,8 @@
+import { getPrisma } from "@/libs/getPrisma";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const prisma = getPrisma();
+  const courses = await prisma.course.findMany();
+  return NextResponse.json({ ok: true, courses });
+};
---
+++ lecture21-2566-starter/src/app/api/user/login/route.js
@@ -0,0 +1,41 @@
+import { getPrisma } from "@/libs/getPrisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+//POST /api/user/login
+
+import { NextResponse } from "next/server";
+
+export const POST = async (request) => {
+  const body = await request.json();
+  const { username, password } = body;
+  
+  const prisma = getPrisma();
+
+  const user = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.password)) {
+    return NextResponse.json(
+      {
+        ok: false,
+        message: "Username or password is incorrect",
+      },
+      { status: 400 }
+    );
+  }
+
+  //read in db here
+
+  //if found user, sign a JWT TOKEN
+  const token = jwt.sign(
+    { username, role: user.role, studentId: user.studentId },
+    process.env.JWT_SECRET,
+    { expiresIn: "8h" }
+  );
+
+  return NextResponse.json({ ok: true, token, username });
+};
